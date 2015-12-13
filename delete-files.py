447ca80db47355968730fe2b356559b3d24458d7
Troy McCall <troyxmccall@gmail.com>
2015-12-13 14:41:07 -0500

    Better delete script

--- delete-files.py
+++ delete-files.py
@@ -1,29 +1,72 @@
 import requests
-import json
-import calendar
 from datetime import datetime, timedelta
+import time
+import sys
 
-# https://api.slack.com/web
-_token = ""
-# your team prefix on *.slack.com
-_domain = ""
 
-if __name__ == '__main__':
-  while 1:
-    files_list_url = 'https://slack.com/api/files.list'
-    date = str(calendar.timegm((datetime.now() + timedelta(-30))
-                               .utctimetuple()))
-    data = {"token": _token, "ts_to": date}
-    response = requests.post(files_list_url, data=data)
-    if len(response.json()["files"]) == 0:
-      break
-    for f in response.json()["files"]:
-      print "Deleting file " + f["name"] + "..."
-      timestamp = str(calendar.timegm(datetime.now().utctimetuple()))
-      delete_url = "https://" + _domain + ".slack.com/api/files.delete?t=" + timestamp
-      requests.post(delete_url, data={
-          "token": _token,
-          "file": f["id"],
-          "set_active": "true",
-          "_attempts": "1"})
-  print "DONE!"
+# Your Settings Here
+WEEKS = 6  # How many weeks old must a file be to be considered.
+MB = 10  # How big must a file be in order to be considered.
+TOKEN = "YOUR-SLACK-TOKEN-HERE"  # Look at https://api.slack.com/web#authentication
+# Set to True to actually delete files, otherwise it's a dry run.
+REALLY_DELETE = False
+
+
+# You shouldn't need to change anything below here.
+threshold = datetime.now() - timedelta(weeks=WEEKS)
+
+unixtime = time.mktime(threshold.timetuple())
+params = {'token': TOKEN, 'ts_to': unixtime, 'page': 1}
+
+print "\nCriteria: Looking for files %s weeks old and larger than %s MB." % (WEEKS, MB)
+
+if not REALLY_DELETE:
+  print "\n########################################  DRY RUN  ########################################"
+  print "  Nothing is really being deleted. Set REALLY_DELETE = True to delete files for realsies."
+  print "###########################################################################################"
+
+print "\nFetching Files with %s" % params
+
+result = requests.get('https://slack.com/api/files.list', params=params)
+json = result.json()
+
+if not json['ok']:
+  print "Is that a valid TOKEN?"
+  print json
+  sys.exit(99)
+
+
+pages = json['paging']['pages']
+files = json['files']
+
+for i in range(2, pages + 1):
+  params['page'] = i
+  print "Fetching Files with %s" % params
+  result = requests.get('https://slack.com/api/files.list', params=params)
+  json = result.json()
+  files += json['files']
+
+print "\n%s files match your age criteria.\n" % len(files)
+
+params = {'token': TOKEN}
+
+total_bytes_deleted = 0
+
+for f in files:
+  if f['size'] > MB * 1000000:
+    print "%s: %s MB" % (f['name'], f['size'] / 1000000),
+
+    if REALLY_DELETE:
+      params['file'] = f['id']
+      a = requests.get('https://slack.com/api/files.delete', params=params)
+      if a.status_code == 200:
+        print " - Deleted"
+        total_bytes_deleted += f['size']
+      else:
+        print " - ERROR: %s " % a.status_code
+        print a.json()
+        print ""
+    else:
+      print " - Matched Age and Size Criteria."
+
+print "\nDeleted %s MB." % (total_bytes_deleted / 1000000)
