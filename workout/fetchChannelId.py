14cad895210e27f57b1f7e832d5623a0e776ed10
Troy McCall <troyxmccall@gmail.com>
2015-12-13 14:33:27 -0500

    Cleanup

--- workout/fetchChannelId.py
+++ workout/fetchChannelId.py
@@ -10,20 +10,20 @@ import os
 import json
 
 # Environment variables must be set with your tokens
-USER_TOKEN_STRING =  os.environ['SLACK_USER_TOKEN_STRING']
-URL_TOKEN_STRING =  os.environ['SLACK_URL_TOKEN_STRING']
+USER_TOKEN_STRING = os.environ['SLACK_USER_TOKEN_STRING']
+URL_TOKEN_STRING = os.environ['SLACK_URL_TOKEN_STRING']
 
 HASH = "%23"
 
 channelName = sys.argv[1]
 
-params = {"token": USER_TOKEN_STRING }
+params = {"token": USER_TOKEN_STRING}
 
 # Capture Response as JSON
 response = requests.get("https://slack.com/api/channels.list", params=params)
 channels = json.loads(response.text, encoding='utf-8')["channels"]
 
 for channel in channels:
-    if channel["name"] == channelName:
-        print channel["id"]
-        break
+  if channel["name"] == channelName:
+    print channel["id"]
+    break
